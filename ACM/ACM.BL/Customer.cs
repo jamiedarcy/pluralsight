65b83c4a9b69eb40fadc284585be0779af7dd2ab
jamiedarcy <jamie.darcy@live.com>
2016-02-12 11:01:56 +0000

    Plurasight training

---
+++ ACM/ACM.BL/Customer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        private string _lastName;
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public static int InstanceCount { get; set; }
+
+
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+
+        public int CustomerID { get; private set; }
+
+        public string FullName
+        {
+            get
+            {
+                if(String.IsNullOrWhiteSpace(FirstName))
+                { 
+                        return LastName;
+                }else if (String.IsNullOrWhiteSpace(LastName))
+                {
+                    return FirstName;
+                }else
+                {
+                    return LastName + ", " + FirstName;
+                }
+
+            }
+
+
+        }
+
+        public bool validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+        }
+    }
+}
---
+++ ACM/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //--Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            customer.LastName = "Baggins";
+
+            string expected = "Baggins, Bilbo";
+
+            //--ACT
+
+            string actual = customer.FullName;
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //--Arrange
+            Customer customer = new Customer();
+            customer.LastName = "Baggins";
+
+            string expected = "Baggins";
+
+            //--ACT
+
+            string actual = customer.FullName;
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //--Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+
+            string expected = "Bilbo";
+
+            //--ACT
+
+            string actual = customer.FullName;
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InstanceCountTest()
+        {
+            //--Arrange
+            var c1 = new Customer();
+            Customer.InstanceCount +=1;
+            var c2 = new Customer();
+            Customer.InstanceCount +=1;
+            var c3 = new Customer();
+            Customer.InstanceCount +=1;
+
+            //-ACT
+            //-Assert
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+
+        [TestMethod]
+        public void ValidateValid()
+        {
+            //--Arrange
+            var c1 = new Customer();
+            c1.LastName = "Baggins";
+            cus
+            var c2 = new Customer();
+            Customer.InstanceCount += 1;
+            var c3 = new Customer();
+            Customer.InstanceCount += 1;
+
+            //-ACT
+            //-Assert
+
+        }
+
+    }
+}
